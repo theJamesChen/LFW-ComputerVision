977f3ae6940fdff38d9fd7338ea899d773f27ba8
James Chen <jchen188@jhu.edu>
2017-11-25 17:02:24 -0500

    updated thresh

--- p1b.py
+++ p1b.py
@@ -366,13 +366,12 @@ def test(testfile, loadPath, gpu, margin):
 		else:
 			prediction = euclideanDistance.data.numpy()
 		#Higher distance = different
-		thresh = 2.0
+		thresh = 1.0
 		#print prediction
-		prediction[prediction > thresh] = 0
-		prediction[prediction <= thresh] = 1
+		prediction[prediction > thresh] = 0.0
+		prediction[prediction <= thresh] = 1.0
 
 		#Batch labels
-		print label.cpu().data.numpy()
 		correct += np.sum(np.equal(prediction, label.cpu().data.numpy()))
 	
 	percentcorrect = float(correct)/Config.batch_size/len(testing_dataloader)
