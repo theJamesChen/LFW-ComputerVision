30656acec8376081da2480392912592fc259f18e
James Chen <jchen188@jhu.edu>
2017-11-25 18:12:54 -0500

    updated thresh

--- p1b.py
+++ p1b.py
@@ -366,7 +366,7 @@ def test(testfile, loadPath, gpu):
 		else:
 			prediction = np.squeeze(euclideanDistance.data.numpy())
 		#Higher distance = different
-		thresh = 1.20
+		thresh = 10
 		prediction[prediction > thresh] = 0
 		prediction[prediction <= thresh] = 1
 
