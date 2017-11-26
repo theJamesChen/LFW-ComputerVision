2362eb4ef969ee59daf81f1ebcc67d5a3fc45f4c
James Chen <jchen188@jhu.edu>
2017-11-26 17:15:27 -0500

    debug

--- p1b.py
+++ p1b.py
@@ -364,7 +364,7 @@ def test(testfile, loadPath, gpu):
 		else:
 			prediction = np.squeeze(euclideanDistance.data.numpy())
 		#Higher distance = different
-		thresh = 10
+		thresh = 2
 		print prediction, label.cpu().numpy()
 		prediction[prediction > thresh] = 0
 		prediction[prediction <= thresh] = 1
