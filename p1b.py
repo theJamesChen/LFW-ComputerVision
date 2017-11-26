ed06f4fff5cc04c1922f91e07c1dfcb11503fe0c
James Chen <jchen188@jhu.edu>
2017-11-26 17:31:14 -0500

    debug

--- p1b.py
+++ p1b.py
@@ -371,7 +371,7 @@ def test(testfile, loadPath, gpu):
 			pred = (euclideanDistance.data.numpy())
 		#Higher distance = different
 		#thresh = 2
-		print pred, label.cpu().numpy()
+		print pred, label.cpu()
 		correct += (pred == label.cpu().numpy()).sum()
 		#print prediction, label.cpu().numpy()
 		#prediction[prediction > thresh] = 0
