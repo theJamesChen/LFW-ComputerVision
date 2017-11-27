887dd1a831cf8ef168405a0e309aea3e6979f08f
James Chen <jchen188@jhu.edu>
2017-11-26 19:33:07 -0500

    debug histogram

--- p1a.py
+++ p1a.py
@@ -405,7 +405,7 @@ def main():
 	else:
 		print "<----------------", "GPU MODE", "---------------->"
 
-	if args.notransform:
+	if args.notransform and args.save:
 		print "<----------------", "Data Augmentation OFF", "---------------->"
 		transform = False
 	else:
